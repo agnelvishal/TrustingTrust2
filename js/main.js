06da24deb8d27c745b9c56e4a687d44c9ac5ae00
agnelvishal <agnelvishal@gmail.com>
2017-12-16 21:14:38 +0530

    default

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var slideshow;
+window.onload = function(){
+
+	// PRELOADER
+	Q.all([
+		Loader.loadAssets(Loader.manifestPreload),
+		Words.convert("words.html")
+	]).then(function(){
+
+		// CHANGE DOM
+		document.body.removeChild($("#preloader"));
+		$("#main").style.display = "block";
+	//	$("#footer").style.display = "block";
+
+		// Slideshow
+		slideshow = new Slideshow({
+			dom: $("#slideshow"),
+			slides: SLIDES
+		});
+
+		// Slide Select
+
+
+
+		subscribe("start/game", function(){
+
+
+
+			// [FOR DEBUGGING]
+			publish("slideshow/next");
+			//publish("slideshow/scratch", ["credits"]);
+
+		});
+
+		// SOUND
+	
+
+		// LOAD REAL THINGS
+		Loader.loadAssets(
+			Loader.manifest,
+			function(){
+				publish("preloader/done");
+			},
+			function(ratio){
+				publish("preloader/progress", [ratio]);
+			}
+		);
+
+		// First slide!
+		slideshow.nextSlide();
+
+	});
+
+};
